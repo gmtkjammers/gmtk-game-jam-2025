d3eeba56b5a22a7de8acd2f64b02bebd94b0ceff
tichaelmurvey <michaelturvey97@gmail.com>
2025-07-31 12:01:33 +0200

    third person camera

---
+++ scenes/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const CAM_SENSITIVITY = 0.01
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		$Pivot.rotate_y(-event.relative.x * CAM_SENSITIVITY)
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_right", "move_left", "move_backward", "move_forward")
+	# var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = ($Pivot.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if input_dir != Vector2.ZERO:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -21,27 +21,27 @@ func _physics_process(_delta: float) -> void:
 	
 	velocity = targetVelocity
 	
-	var mouse_pos: Vector3 = _get_mouse_position()
-	print("mouse position: ",mouse_pos)
+	# var mouse_pos: Vector3 = _get_mouse_position()
+	# print("mouse position: ",mouse_pos)
 	
 	
-	$Pivot.look_at(mouse_pos, Vector3.UP, true)
-	$Pivot.rotation_degrees.x = 0
+	# $Pivot.look_at(mouse_pos, Vector3.UP, true)
+	# $Pivot.rotation_degrees.x = 0
 	
 	move_and_slide()
 
-func _get_mouse_position() -> Vector3:
-	var mouse_position = get_viewport().get_mouse_position()
-	var camera: Camera3D = get_node("../CameraPivot/Camera3D")
+# func _get_mouse_position() -> Vector3:
+# 	var mouse_position = get_viewport().get_mouse_position()
+# 	var camera: Camera3D = get_node("../CameraPivot/Camera3D")
 	
-	var ray_origin = camera.project_ray_origin(mouse_position)
-	var ray_target = ray_origin + camera.project_ray_normal(mouse_position) * 2000
+# 	var ray_origin = camera.project_ray_origin(mouse_position)
+# 	var ray_target = ray_origin + camera.project_ray_normal(mouse_position) * 2000
 	
-	var space_state = get_world_3d().direct_space_state
-	var intersection = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(ray_origin, ray_target,1))
+# 	var space_state = get_world_3d().direct_space_state
+# 	var intersection = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(ray_origin, ray_target,1))
 	
-	if not intersection.is_empty():
-		var pos = intersection.position
-		return pos
+# 	if not intersection.is_empty():
+# 		var pos = intersection.position
+# 		return pos
 	
-	return Vector3.ZERO
+# 	return Vector3.ZERO
