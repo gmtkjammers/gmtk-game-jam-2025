b2ad75841f080ed91e92c800121f96a7766fec95
Rodrigo Sonego <rodrigoasonego@gmail.com>
2025-08-01 22:26:19 -0300

    destroy bullet on impact with player

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -15,6 +15,7 @@ func _physics_process(delta: float) -> void:
 		for body in get_overlapping_bodies():
 			if body is CharacterBody3D:
 				body.take_damage()
+				queue_free()
 
 func shoot(direction: Vector3):
 	will_move = true
